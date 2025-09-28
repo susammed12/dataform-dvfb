1abc2f7113478d95355dbaffe829e2a17dd437fd
Suraj Awati <susammed12@gmail.com>
2025-09-28 17:12:24 +0000

    added wrapper for AISJ project

---
+++ definitions/wrapper.js
@@ -0,0 +1,49 @@
+declare({
+  schema: "staging",
+  name: "AI_AIRPORT_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "AI_BOOKING_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "AI_FLIGHT_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "AI_PASSENGER_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "AI_PAYMENT"
+});
+
+declare({
+  schema: "staging",
+  name: "SJ_AIRPORT_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "SJ_BOOKING_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "SJ_FLIGHT_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "SJ_PASSENGER_DETAILS"
+});
+
+declare({
+  schema: "staging",
+  name: "SJ_PAYMENT"
+});
