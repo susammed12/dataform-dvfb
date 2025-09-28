bc1cc056cf7f0511f5013384ffa0fa4c6b22709f
Suraj Awati <susammed12@gmail.com>
2025-09-28 19:17:35 +0000

    NA

--- definitions/generator.js
+++ definitions/generator.js
@@ -1,45 +1,110 @@
-
-const fs = require('fs');
-
 const metadata = {
   "hubs": [
     {
-      "table_name": "FLIGHT",
-      "business_key": "FlightID",
-      "source_table": "AI_FLIGHT_DETAILS"
+      "entity": "Passenger",
+      "business_key": "PassengerID",
+      "source_table": "staging_passenger"
     },
     {
-      "table_name": "AIRPORT",
-      "business_key": "AirportCode",
-      "source_table": "AI_AIRPORT_DETAILS"
+      "entity": "Flight",
+      "business_key": "FlightID",
+      "source_table": "staging_flight"
     }
-
-]
+  ],
+  "links": [
+    {
+      "link_name": "Booking",
+      "keys": [
+        "PassengerID",
+        "FlightID",
+        "PaymentID"
+      ],
+      "source_table": "staging_booking"
+    }
+  ],
+  "satellites": [
+    {
+      "satellite_name": "PassengerDetails",
+      "parent_key": "PassengerID",
+      "attributes": [
+        "FirstName",
+        "LastName",
+        "DOB",
+        "Email"
+      ],
+      "source_table": "staging_passenger"
+    }
+  ]
 };
 
-function generateHub(table_name, business_key, source_table) {
+function generateHub(entity, businessKey, sourceTable) {
   return `
 config {
   type: "table",
   tags: ["hub"]
 }
 
+CREATE OR REPLACE TABLE \`project.dataset.hub_${entity.toLowerCase()}\` AS
 SELECT
-  MD5(${business_key}) AS HK_${business_key},
-  ${business_key},
-  CURRENT_TIMESTAMP() AS LOAD_DTS,
-  '${source_table}' AS REC_SRC
+  MD5(${businessKey}) AS Hub_${entity}_Key,
+  ${businessKey},
+  CURRENT_TIMESTAMP() AS LoadDate,
+  '${sourceTable}' AS RecordSource
+FROM \`project.dataset.${sourceTable}\`
+WHERE ${businessKey} IS NOT NULL;
+`;
+}
 
-FROM ${source_table}
-WHERE ${business_key} IS NOT NULL;
+function generateLink(linkName, keys, sourceTable) {
+  const linkKey = keys.map(k => `CAST(${k} AS STRING)`).join(", ");
+  const hubKeys = keys.map(k => `MD5(${k}) AS Hub_${k}_Key`).join(",\n  ");
+  return `
+config {
+  type: "table",
+  tags: ["link"]
+}
+
+CREATE OR REPLACE TABLE \`project.dataset.link_${linkName.toLowerCase()}\` AS
+SELECT
+  MD5(CONCAT(${linkKey})) AS Link_${linkName}_Key,
+  ${hubKeys},
+  CURRENT_TIMESTAMP() AS LoadDate,
+  '${sourceTable}' AS RecordSource
+FROM \`project.dataset.${sourceTable}\`
+WHERE ${keys.map(k => `${k} IS NOT NULL`).join(" AND ")};
+`;
+}
+
+function generateSatellite(satName, parentKey, attributes, sourceTable) {
+  const attrList = attributes.join(",\n  ");
+  return `
+config {
+  type: "table",
+  tags: ["satellite"]
+}
+
+CREATE OR REPLACE TABLE \`project.dataset.sat_${satName.toLowerCase()}\` AS
+SELECT
+  MD5(${parentKey}) AS Hub_${parentKey}_Key,
+  ${attrList},
+  CURRENT_TIMESTAMP() AS LoadDate,
+  '${sourceTable}' AS RecordSource
+FROM \`project.dataset.${sourceTable}\`;
 `;
 }
 
 const sqlxScripts = [];
 
 metadata.hubs.forEach(hub => {
-  sqlxScripts.push(generateHub(hub.table_name, hub.business_key, hub.source_table));
+  sqlxScripts.push(generateHub(hub.entity, hub.business_key, hub.source_table));
 });
 
-// Write to a .sqlx file
-fs.writeFileSync("generated_hubs.sqlx", sqlxScripts.join("\n\n"));
+metadata.links.forEach(link => {
+  sqlxScripts.push(generateLink(link.link_name, link.keys, link.source_table));
+});
+
+metadata.satellites.forEach(sat => {
+  sqlxScripts.push(generateSatellite(sat.satellite_name, sat.parent_key, sat.attributes, sat.source_table));
+});
+
+sqlxScripts.forEach(script => console.log(script));
