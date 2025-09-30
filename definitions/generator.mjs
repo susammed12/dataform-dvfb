ec0dce7071c66cc8adcc6b92ac7ed919c15bad41
Suraj Awati <susammed12@gmail.com>
2025-09-30 12:23:22 +0000

    NA

--- definitions/generator.mjs
+++ definitions/generator.mjs
@@ -60,7 +60,7 @@ GROUP BY ${business_key}
 
 metadata.hubs.forEach(hub => {
   const script = generateHub(hub.table_name, hub.table_type, hub.business_key, hub.source_table_AI, hub.source_table_SJ);
-  const fileName = `${table_type}_${hub.table_name.toupperCase()}.sqlx`;
+  const fileName = `${hub.table_type}_${hub.table_name.toUpperCase()}.sqlx`;
   const filePath = path.join(targetDir, fileName);
   fs.writeFileSync(filePath, script);
   console.log(`✅ SQLX file '${filePath}' has been created.`);
